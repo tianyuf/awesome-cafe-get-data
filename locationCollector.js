696b54b367b4154a3435be7380b1069367778e0f
Alex Cai <itaieps+github@fastmail.fm>
2016-02-22 22:58:00 +0800

    Update locationCollector.js

--- locationCollector.js
+++ locationCollector.js
@@ -1,22 +1,20 @@
 function getLocation() {
-  var venueName = document.getElementsByClassName("venueName")[0].innerHTML;
+  var venueName = document.querySelector("venueName").innerHTML;
   var venueLatitude = document.getElementsByTagName("meta")[23].content;
   var venueLongitude = document.getElementsByTagName("meta")[24].content;
   var venueCoordinates = `${venueLatitude}, ${venueLongitude}`;
-  var createGeojson =
-        `
-        {
-          "type": "Feature",
-          "properties": {
-            "名称": "${venueName}",
-          }
-          "geometry": {
-            "type": "Point",
-            "coordinates": [
-              ${venueLatitude},
-              ${venueLongitude}
-            ]
-        }`
-  return(createGeojson);
+  return JSON.stringify({
+    "type": "Feature",
+    "properties": {
+      "名称": venueName,
+    }
+    "geometry": {
+      "type": "Point",
+      "coordinates": [
+        venueLatitude,
+        venueLongitude
+      ]
+    }
+  });
 }
 getLocation();
